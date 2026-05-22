5464d3a459bdd2fa7a3e65ad2936f12c210f92b6
Salisou <salisoumoussa1@gmail.com>
2026-05-22 14:03:46 +0200

    Added comments

--- 22_05_2026.sql
+++ 22_05_2026.sql
@@ -11,23 +11,43 @@ WHERE TABLE_TYPE = 'BASE TABLE'
 	Operatori di confronto
 
 	Servono per confrontare valori:
-		= → uguale
-		!= o <> → diverso
-		> → maggiore
-		< → minore
-		>= → maggiore o uguale
-		<= → minore o uguale
+		| Operatore | Significato       |
+		| --------- | ----------------- |
+		| `=`       | uguale            |
+		| `!=`      | diverso           |
+		| `<>`      | diverso           |
+		| `>`       | maggiore          |
+		| `<`       | minore            |
+		| `>=`      | maggiore o uguale |
+		| `<=`      | minore o uguale   |
+
 */
+
+-- Esempio — studenti nati dopo il 2000
+SELECT *
+FROM Studenti
+WHERE DataNascita > '2000-01-01';
 
 
 select * from Studenti
 
 
 -- Operatori logici 
--- AND → tutte le condizioni devono essere vere
--- OR → almeno una vera
--- NOT → nega la condizione
--- IS NULL
+/*
+	| Operatore | Significato        |
+	| --------- | ------------------ |
+	| `AND`     | tutte vere         |
+	| `OR`      | almeno una vera    |
+	| `NOT`     | nega la condizione |
+	| `IS NULL` | valore nullo       |
+
+*/
+
+SELECT 
+    Nome + ' ' + Cognome AS 'Nome completo',
+    Telefono
+FROM Studenti
+WHERE DataNascita IS NULL;
 
 SELECT 
 	Nome + ' ' + Cognome AS 'Nome completo',
@@ -38,7 +58,14 @@ WHERE DataNascita IS NULL AND Nome = 'Mario'
 
 
 
+/*
+Spiegazione
 
+La query restituisce:
+	studenti chiamati Mario
+	E con DataNascita mancante
+	Entrambe le condizioni devono essere vere.
+*/
 SELECT 
 	Nome + ' ' + Cognome AS 'Nome completo',
 	CodiceFiscale,
@@ -48,7 +75,24 @@ FROM Studenti
 WHERE DataNascita >= '1985' AND DataNascita <= '2000'
 ORDER BY DataNascita ASC
 
+/*
+	Spiegazione della Concattenazione
+		Unisce Nome e Cognome.
+		AS(Alias) rinomina la colonna nel risultato.
+*/
 
+/*
+	La query sotto è equivalente a:
+	SELECT 
+		Nome + ' ' + Cognome AS 'Nome completo',
+		CodiceFiscale,
+		Telefono
+	FROM Studenti
+	WHERE Nome = 'Mario'
+	   OR Nome = 'Luca'
+	   OR Nome = 'Giulia'
+	   OR Nome = 'Chiara'
+*/
 SELECT 
 	Nome + ' ' + Cognome AS 'Nome completo',
 	CodiceFiscale,
@@ -57,15 +101,27 @@ FROM Studenti
 WHERE Nome IN ('Mario', 'Luca', 'Giulia','Chiara')
 
 
+/*
+	IN con ID
+	Permette di cercare più valori.
+
+	Spiegazione del Order by 
+		Ordina dalla data più vecchia alla più recente.
+		| Keyword | Significato |
+		| ------- | ----------- |
+		| `ASC`   | crescente   |
+		| `DESC`  | decrescente |
+*/
+
 SELECT 
 	Nome + ' ' + Cognome AS 'Nome completo',
 	CodiceFiscale,
 	Telefono
 FROM Studenti
 WHERE StudenteId IN (10, 20, 8 ,7, 1)
-ORDER BY Nome ASC
+ORDER BY Nome ASC -- Ordinamento con ORDER BY
 
-SELECT DISTINCT
+SELECT DISTINCT -- Rimuove i duplicati
 	StudenteId,
 	Nome + ' ' + Cognome AS 'Nome completo'
 FROM Studenti;
@@ -79,15 +135,21 @@ FROM Corsi
 WHERE Crediti BETWEEN 10 AND 50
 ORDER BY Crediti ASC;
 
--- LIKE 
--- M% 
--- %O 
--- %A%
--- _ 
+/*
+	Serve per cercare testi.
+	| Pattern | Significato   |
+	| ------- | ------------- |
+	| `M%`    | inizia con M  |
+	| `%o`    | finisce con o |
+	| `%a%`   | contiene a    |
+	| `_`     | un carattere  |
+
+*/
 SELECT 
 	Nome + ' ' + Cognome AS 'Nome completo',
 	CodiceFiscale,
 	Telefono
 FROM Studenti
-WHERE Nome LIKE 'M%' or Nome LIKE '%a'
+WHERE Nome LIKE 'M%' -- nomi che iniziano con M
+	or Nome LIKE '%a' -- oppure finiscono con a
 
